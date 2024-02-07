030aa054124f408648dd8c1f5a60ca233a88ef6f
Faith <wachiraafaith@gmail.com>
2024-02-07 21:52:05 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){
+    //var myName: String = "Faith"
+    //var lightsOn = true
+
+    //arrayOf("Tanzania", "Rwanda", "Uganda") ctrl + /
+
+    var fullName = "Faith Hope"
+    println(fullName)
+    var age = "20"
+    println(age)
+    var phoneNumber = "0711303757"
+    println(phoneNumber)
+    var weightInKg = "50"
+    println(weightInKg)
+    var citizen = "Kenyan"
+    println(citizen)
+
+
+}
